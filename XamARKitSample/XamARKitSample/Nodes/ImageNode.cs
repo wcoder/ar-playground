5b80df5d88c465a11278937e86c496c835a25de3
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-04-24 17:42:41 +0300

    Add simple image

---
+++ XamARKitSample/XamARKitSample/Nodes/ImageNode.cs
@@ -0,0 +1,42 @@
+﻿using Foundation;
+using SceneKit;
+using UIKit;
+
+namespace XamARKitSample.Nodes
+{
+    public class ImageNode : SCNNode
+    {
+        public ImageNode(string imageUrl, float width, float height)
+        {
+            var node = new SCNNode
+            {
+                Geometry = CreateGeometry(imageUrl, width, height),
+            };
+            AddChildNode(node);
+        }
+
+        private SCNGeometry CreateGeometry(string imageUrl, float width, float height)
+        {
+            var image = imageUrl.StartsWith("http")
+                ? FromUrl(imageUrl)
+                : UIImage.FromFile(imageUrl);
+
+            var material = new SCNMaterial();
+            material.Diffuse.Contents = image;
+            material.DoubleSided = true;
+
+            var geometry = SCNPlane.Create(width, height);
+            geometry.Materials = new[] { material };
+
+            return geometry;
+        }
+
+        private static UIImage FromUrl(string resource)
+        {
+            using var url = new NSUrl(resource);
+            using var imageData = NSData.FromUrl(url);
+
+            return UIImage.LoadFromData(imageData);
+        }
+    }
+}
--- XamARKitSample/XamARKitSample/ViewController.cs
+++ XamARKitSample/XamARKitSample/ViewController.cs
@@ -1,12 +1,16 @@
 ﻿using ARKit;
+using SceneKit;
 using System;
 using UIKit;
+using XamARKitSample.Nodes;
 
 namespace XamARKitSample
 {
     public partial class ViewController : UIViewController
     {
         private ARSCNView _sceneView;
+
+        const float zPosition = -0.25f;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -35,6 +39,14 @@ namespace XamARKitSample
                 LightEstimationEnabled = true,
                 WorldAlignment = ARWorldAlignment.Gravity
             }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
+
+            var imageUrl = "https://raw.githubusercontent.com/dotnet/brand/main/logo/dotnet-logo.png";
+            var imageNode = new ImageNode(imageUrl, 0.1f, 0.1f)
+            {
+                Position = new SCNVector3(0, 0, zPosition)
+            };
+
+            _sceneView.Scene.RootNode.AddChildNode(imageNode);
         }
 
         public override void ViewDidDisappear(bool animated)
